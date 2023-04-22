54bf365ad2e4491bf811d920169239c1c2bc85d0
RayZarei <ahmadreza0zarei@gmail.com>
2023-04-22 17:01:19 +0300

    Add ViewModel Module

---
+++ app/src/main/java/com/ray/zarei/musicplayer/di/modules/ViewModelModule.kt
@@ -0,0 +1,46 @@
+package com.ray.zarei.musicplayer.di.modules
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import dagger.Binds
+import dagger.MapKey
+import dagger.Module
+import dagger.multibindings.IntoMap
+import javax.inject.Inject
+import javax.inject.Provider
+import javax.inject.Singleton
+import kotlin.reflect.KClass
+
+@Singleton
+class ViewModelFactory @Inject constructor(
+    private val viewModels: MutableMap<Class<out ViewModel>,
+            @JvmSuppressWildcards Provider<ViewModel>>
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T =
+        viewModels[modelClass]?.get() as T
+}
+
+
+@Target(
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.PROPERTY_GETTER,
+    AnnotationTarget.PROPERTY_SETTER
+)
+@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
+@MapKey
+internal annotation class ViewModelKey(val value: KClass<out ViewModel>)
+
+
+@Module
+abstract class ViewModelModule {
+
+    @Binds
+    abstract fun bindViewModelFactory(factory: ViewModelFactory): ViewModelProvider.Factory
+//
+//    @Binds
+//    @IntoMap
+//    @ViewModelKey(HomeRecipesViewModel::class)
+//    abstract fun bindHomeRecipesViewModel(viewModel: HomeRecipesViewModel): ViewModel
+//
+}
+
